e71cd9456ee55c5de34b53732b2b9da356fa1629
voronoijay <jaeheon12@voronoi.io>
2021-11-01 17:46:24 +0900

    aaa

--- management/admin.py
+++ management/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import StringData
 
 class StringDataAdmin(admin.ModelAdmin):
-    pass
+    show_full_result_count = False
 
 admin.site.register(StringData, StringDataAdmin)
